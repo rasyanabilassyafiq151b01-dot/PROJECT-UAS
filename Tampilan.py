94c59c531ca35b70b2ba758c0e650ccb7bdbbd0d
unknown <rasyanabilassyafiq151b01@gmail.com>
2025-12-29 18:51:24 +0700

    PROJECT1

--- Tampilan.py
+++ Tampilan.py
@@ -1,9 +1,16 @@
 db = []
 
 print("""
-==============================
-        SELAMAT DATANG
-    DI RENTAL MOBIL BAROKAH
-==============================
+            ==============================
+                    SELAMAT DATANG
+                DI RENTAL MOBIL BAROKAH
+            ==============================
+
+        SILAHKAN PILIH SEAT YANG ANDA PERLUKAN:
+        
+         1. 4 SEAT    2. 6 SEAT     3. 8 SEAT 
+
+
 """)
+
 
